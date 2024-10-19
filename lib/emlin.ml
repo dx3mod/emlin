011b9735cdc54898dec7a5afeead03292271421f
dx3mod <dx3mod@bk.ru>
2024-10-19 07:45:32 +0300

    Add protect with_sysfs_gpio fun

--- lib/emlin.ml
+++ lib/emlin.ml
@@ -4,10 +4,10 @@ module I2c = I2c
 let dev_flag =
   Sys.getenv_opt "DEV" |> Option.fold ~none:false ~some:(( = ) "sim")
 
-let with_sysfs_gpio f =
+let with_sysfs_gpio ?(catch_break = true) f =
   let module Gpio = Sysfs.Gpio.Make (Sysfs.Gpio.Hardware) in
-  f (module Gpio : Sysfs.Gpio.S);
-  Gpio.unexport ()
+  Sys.catch_break catch_break;
+  Fun.protect ~finally:Gpio.unexport (fun () -> f (module Gpio : Sysfs.Gpio.S))
 
 let with_i2c f =
   let module I = I2c.Make (I2c.Hardware) in
